548edcab71bf98ad67b7bdeb7b164fbfce5655cf
Akila Weeratunga <akilaweeratunga@gmail.com>
2015-05-11 23:04:20 +0530

    added file upload stuff

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -11,4 +11,37 @@ class TestController extends BaseController {
 	{
 		return View::make('layouts/userMaster');
 	}
+
+	public function processUpload()
+	{
+		$file = Input::file('file'); // your file upload input field in the form should be named 'file'
+
+		$rules = array(
+  		  'file' => 'mimes:jpeg,bmp,png,pdf,doc,docx'
+		);
+
+		$inputs = array('file' => $file);
+ 
+		$validation = Validator::make($inputs, $rules);
+
+		if($validation->passes())
+		{
+			$destinationPath = public_path().'/uploads/';
+			$filename        = str_random(6) . '_' . $file->getClientOriginalName();
+	        $uploadSuccess   = $file->move($destinationPath, $filename);
+			if( $uploadSuccess ) 
+			{
+			   $sha1file = hash_file('sha256', $destinationPath.'/'.$filename);
+			   echo $sha1file;// or do a redirect with some message that file was uploaded
+			} 
+			else 
+			{
+			   return Response::json('error', 400);
+			}
+		}
+		else
+		{
+			return 'sorry , file type is not supported';
+		}
+	}
 }
