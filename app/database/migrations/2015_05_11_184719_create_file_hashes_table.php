03d6f0ce1e0d7c776dff8565aaa636fe32c43edb
Isuru Walpola <iwalpola@gmail.com>
2015-05-12 00:59:02 +0530

    migration for file hashes

---
+++ app/database/migrations/2015_05_11_184719_create_file_hashes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileHashesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('filehashes', function(Blueprint $table)
+		{
+			$table->increments('file_id');
+			$table->string('hash',32);
+			$table->integer('contract_id')->length(12)->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('filehashes');
+	}
+
+}
