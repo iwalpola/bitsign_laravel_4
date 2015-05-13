7ebaae5cdeed985b64d6b30a568c2f6618d99152
Gamunu Balagalla <gamunuud@gmail.com>
2015-05-13 13:45:54 +0530

    New login form and styles

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -6,29 +6,29 @@
 <section class="section login-container offset-header">
 	<div class="container">
     	<div class="login-form">
-    		<center>
-	    	<h1 class="login-title"><img src="/img/logo.png">Login</h1> <br>
-	    	@if (Session::get('invalid') == true)
-	    	<div class="alert alert-danger">
-	    	Incorrect username/password combination
-	    	</div>
-	    	@endif
-		      	{{ Form::open(array('route' => 'sessions.store')) }}
-		      			<div class="form-group">
-			      			{{Form::label('username','Username:')}}
-			      			<br>
-			      			{{Form::text('username')}}
-		      			</div>
-		      			<div class="form-group">
-			      			{{Form::label('password','Password:')}}
-			      			<br>
-			      			{{Form::password('password')}}
-			      		</div>
-			      		<div class="form-group">
-			      			{{Form::submit('Login', array('class'=>'btn btn-cta-secondary'))}}
-			      		</div>
-				{{ Form::close() }}
-			</center>
+			<div class="card card-container">
+	    		<center>
+			    	<h1 class="login-title"><img src="/img/logo.png">Login</h1> <br>
+			    	@if (Session::get('invalid') == true)
+			    	<div class="alert alert-danger">
+			    	Incorrect username/password combination
+			    	</div>
+			    	@endif
+				      	{{ Form::open(array('route' => 'sessions.store')) }}
+				      			<div class="form-group">
+					      			{{--Form::label('username','Username:')--}}
+					      			{{Form::text('username', Input::old('username'), ['class' => 'form-control', 'placeholder' => 'Username', 'required', 'autofocus'])}}
+				      			</div>
+				      			<div class="form-group">
+					      			{{--Form::label('password','Password:')--}}
+					      			{{Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password', 'required'])}}
+					      		</div>
+					      		<div class="form-group">
+					      			{{Form::submit('Login', array('class'=>'btn btn-lg btn-cta-secondary btn-block'))}}
+					      		</div>
+						{{ Form::close() }}
+				</center>
+			</div>
 		</div>
 	</div>
 </section>
